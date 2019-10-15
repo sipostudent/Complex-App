0b0c04046cc367db989871437389ff8c500d4a8e
Sipo Charles <sipocharles@yahoo.co.uk>
2019-10-15 18:16:15 +0100

    connected app to database

---
+++ db.js
@@ -0,0 +1,9 @@
+const mongodb = require('mongodb')
+
+const connectionString = 'mongodb+srv://?@cluster0-0oagu.gcp.mongodb.net/ComplexApp?retryWrites=true&w=majority'
+
+mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
+  module.exports = client.db()
+  const app = require('./app')
+  app.listen(3000)
+})
